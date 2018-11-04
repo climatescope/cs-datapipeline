934fdd2fb33b77f10e3f7aae3878ece7dc5939d0
Olaf Veerman <olaf.veerman@gmail.com>
2018-11-04 04:26:11 -0500

    Add test framework and first tests for chart definitions

---
+++ tests/test-inputdata.js
@@ -0,0 +1,32 @@
+'use strict'
+const assert = require('chai').assert
+const fs = require('fs-extra')
+const parse = require('neat-csv')
+
+function loadCSV (path) {
+  return parse(fs.readFileSync(path))
+}
+
+describe('Input Data', function () {
+  describe('Charts', async () => {
+    const fp = './input/definitions/charts.csv'
+
+    it('the definition file exists', async () =>
+      assert.isTrue(await fs.pathExists(fp), `${fp} does not exist`)
+    )
+
+    it('has all the required headers', async () => {
+      const requiredHeaders = [ 'id', 'indicatorId', 'name', 'type', 'description', 'labelX', 'labelY' ]
+      const data = await loadCSV(fp)
+
+      return assert.containsAllKeys(data[0], requiredHeaders, `The chart definition doesn't require one of the required headers`)
+    })
+
+    it('valid chart types', async () => {
+      const validChartTypes = ['singleAnswer', 'timeSeries']
+      const data = await loadCSV(fp)
+
+      return assert.isTrue(data.map(c => c.type).every(r => validChartTypes.includes(r)), `The chart definition contains invalid chart types. Should be one of ${validChartTypes}`)
+    })
+  })
+})

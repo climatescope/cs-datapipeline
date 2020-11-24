8829b22dfd4a2a07d0d0b35ced3fabb418d922a1
Junior flores <junior@developmentseed.org>
2020-11-24 16:48:51 -0500

    update test

--- tests/test-processdata.js
+++ tests/test-processdata.js
@@ -8,12 +8,13 @@ const process = require('../scripts/process-data')
 
 describe('Process Data', function () {
   describe('Geographies', async () => {
-    let inputGeographies = [{ id: 'UY', name: 'Uruguay', grid: 'off', region: 'lac' }]
+    let inputGeographies = [{ id: 'UY', name: 'Uruguay', grid: 'off', region: 'lac', market_grouping: 'developing market' }]
     let inputRegions = [{ id: 'lac', name: 'Latin America and the Caribbean' }]
     let expected = [{
       iso: 'uy',
       name: 'Uruguay',
       grid: 'off',
+      market_grouping: 'developing market',
       region: {
         id: 'lac',
         name: 'Latin America and the Caribbean'
